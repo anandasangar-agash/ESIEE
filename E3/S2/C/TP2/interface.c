2ea85df08e5e890e68294e998a9fd1bcdce6d83a
Agash Anandasangar <agash95@live.com>
2026-03-27 23:54:25 +0100

    TP2 C

--- E3/S2/C/TP2/interface.c
+++ E3/S2/C/TP2/interface.c
@@ -75,7 +75,7 @@ void jouer_interface(Plateau *P){
 
         dessiner_plateau(P, morceaux, w, h);
 
-        if(est_resolu(P)) {
+        if(PlateauGagne(P)) {
             MLV_draw_text(10, 10, "GAGNE !", MLV_COLOR_RED);
             MLV_actualise_window();
             MLV_wait_seconds(2);
